44e4e232f4d6761022c6d7f852445a37df43f729
GuilhermePontes1 <guilhermepontes58@hotmail.com>
2021-05-05 01:57:09 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import DataTable from "componentes/DataTable";
+import Footer from "componentes/Footer";
+import NavBar from "componentes/NavBar";
+
 function App() {
   return (
-    <div>
+    <> 
+    <NavBar/>
+    <div className="container">
         <h1 className="text-primary">Olá mundo</h1>
+        <DataTable/>
     </div>
-  );
+        <Footer/>
+    </>
+ );
 }
-
+// utliza para delimitar a função como uma só 
 export default App;
